9247ea21de5f64c74e1901bc137251dec9a0f2be
Tanner <tannerohio@gmail.com>
2025-04-02 16:22:05 -0400

    q13

--- questions.py
+++ questions.py
@@ -251,5 +251,24 @@ def execute_q12():
 
     print(evens)
 
-execute_q12()
+#execute_q12()
 
+"""
+Level 2
+
+Question: Write a program that accepts a sentence and calculate the number of letters and digits. 
+Suppose the following input is supplied to the program: hello world! 123 Then, the output should be: LETTERS 10 DIGITS 3
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+"""
+def execute_q13():
+    sentence = input("Please enter a sentence: ")
+    chara, ints = 0 , 0
+    for char in sentence:
+        if char.isdigit():
+            ints += 1
+        elif char.isalpha():
+            chara += 1
+    print(f"LETTERS  {chara}  DIGITS  {ints}")
+
+#execute_q13()
